58559fb66775fb2413fcef0613cbe54d3a5fb297
rez1-dev <rezwan.mahmud.007@gmail.com>
2025-08-25 13:09:46 +0600

    Payoo mobile App

---
+++ js/home.js
@@ -0,0 +1,103 @@
+const validPin = 1234;
+
+// add money feature
+
+document.getElementById("add-money-btn").addEventListener("click", function (e) {
+    e.preventDefault();
+    const bank = document.getElementById("bank").value;
+    const accountNumber = document.getElementById("account-number").value;
+
+    const amount = parseInt(document.getElementById("add-amount").value);
+
+    const pinNumber = parseInt(document.getElementById("add-pin").value);
+
+    const availableBalance = parseInt(
+      document.getElementById("available-balance").innerText
+    );
+
+    if (accountNumber.length !== 11) {
+      alert("Please Provide Valid Account Number");
+      return;
+    }
+
+    if (pinNumber !== validPin) {
+      alert("Please Provide valid Pin Number");
+      return;
+    }
+
+    const totalNewAvailableBalance = amount + availableBalance;
+    document.getElementById("available-balance").innerText =
+      totalNewAvailableBalance;
+  });
+
+
+
+  //CashOut money feature
+
+  document.getElementById('withdraw-btn').addEventListener('click',function(e){
+    e.preventDefault();
+    // console.log('clicked')
+    
+    const amount = parseInt(document.getElementById('withdraw-amount').value);
+
+    const availableBalance = document.getElementById('available-balance').innerText;
+
+    const totalNewAvailableBalance = availableBalance - amount;
+    document.getElementById('available-balance').innerText = totalNewAvailableBalance;
+
+     const agentNumber = parseInt(document.getElementById('agent-number').value);
+
+    const pinNumber = parseInt(document.getElementById('pin').value)
+
+    if(agentNumber.length !== 11){
+        alert("Please Provide Valid Agent Number")
+        return;
+    }
+    if(pinNumber !== validPin){
+        alert("Please Provide valid Pin");
+        return;
+    }
+});
+
+//Get bonus feature
+
+document.getElementById('get-bonus-btn').addEventListener('click',function(e){
+    e.preventDefault();
+    const coupon = 8888;
+    const couponValue = parseInt(document.getElementById('coupon-input').value)
+    
+    const available = document.getElementById('available-balance').innerText
+    const intAvailable = parseInt(available);
+    if(coupon===couponValue){
+        available1 = intAvailable + 1000;
+        document.getElementById('available-balance').innerText = available1;
+    }
+    else{
+        alert('Please Provide valid Coupon');
+    }
+
+   
+});
+
+
+  //toggling feature--
+
+  document.getElementById("add-button").addEventListener("click", function (e) {
+     document.getElementById("cash-out-parent").style.display = "none";
+      document.getElementById('get-bonus-parent').style.display = 'none';
+  document.getElementById("add-money-parent").style.display = "block";
+});
+document
+  .getElementById("cash-out-button")
+  .addEventListener("click", function () {
+    document.getElementById("add-money-parent").style.display = "none";
+    document.getElementById("cash-out-parent").style.display = "block";
+     document.getElementById('get-bonus-parent').style.display = 'none';
+  });
+
+  document.getElementById('get-bonus').addEventListener('click',function(e){
+    document.getElementById('add-money-parent').style.display = 'none'
+    document.getElementById('cash-out-parent').style.display = 'none';
+    document.getElementById('get-bonus-parent').style.display = 'block';
+  })
+
---
+++ js/index.js
@@ -0,0 +1,20 @@
+//Login button functionality
+
+document.getElementById('btn-login').addEventListener('click',function(e){
+   e.preventDefault();
+  
+
+   const mobileNumber = +8801737539282;
+   const pin = 1234;
+   const mobileNumberValue = parseInt(document.getElementById('mobile-number-value').value );
+   const pinNumberValue = parseInt(document.getElementById('pin-value').value) ;
+
+   if(mobileNumber===mobileNumberValue && pin===pinNumberValue){
+      window.location.href = '/home.html';
+   }
+   else{
+      alert('Wrong credentials!!');
+   }
+
+});
+
